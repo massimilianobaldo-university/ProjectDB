d5b973bc38d8e7aec97cfdba953eb603911031db
zucchi99 <francesco.zuccato99@gmail.com>
2021-05-10 18:40:54 +0200

    scriptino per eseguire sql da bash

---
+++ run_sql.sh
@@ -0,0 +1,14 @@
+nOfParamsNeeded=2
+
+if test $# -lt $nOfParamsNeeded
+then
+    echo "usage: $0 <user> <db_name> <file.sql>"
+    echo "example: $0 postgres progettobasididatidb query.sql"
+    exit 1
+fi
+
+user=$1
+db=$2
+filesql=$3
+
+sudo -u $user psql -d $db -a -f $filesql
