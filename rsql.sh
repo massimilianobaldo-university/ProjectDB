0b77f3a3c14873034973c5997c86d320222b5b14
Massimiliano <bmaxium01@gmail.com>
2021-06-30 22:59:08 +0200

    More elegant scrpit

---
+++ rsql.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# rsql - Bash shell script for run sql files in a Postgres database
+# Usage: rsql [[-h|--help]
+#        rsql [-u|--user] [-d|--databse] [query.sql]]
+# ---------------------------------------------------------------------------
+
+user='postgres'
+db='progettobasididatidb'
+
+# Parse command line
+while [[ -n $1 ]]; do
+  case $1 in
+    -h | --help)
+      echo "rsql - Bash shell script for run sql files in a Postgres database";
+      echo -e "Usage:\trsql [[-h|--help]";
+      echo -e "\trsql [-u|--user] [-d|--databse] [query.sql]]";
+      exit 1;
+      ;;
+    -u | --user)
+      user=$2;
+      shift;
+      ;;
+    -d | --databse)
+      db=$2;
+      shift;
+      ;;
+    -* | --*)
+      echo Unknow flas;
+      ;;
+    *)
+      fileSQL=$1; break
+      ;;
+  esac
+  shift
+done
+
+
+if [[ $user != 'postgres' || $db != 'progettobasididatidb' ]]; then
+    psql -U $user -d $db -a -f $fileSQL
+else 
+    psql -U $user -a -f $fileSQL
+fi
