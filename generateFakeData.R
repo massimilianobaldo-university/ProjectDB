3a9e60470ad52714a6eedff81dd4929930f177d9
Massimiliano <bmaxium01@gmail.com>
2021-07-06 13:07:01 +0200

    Add more useful functions

--- generateFakeData.R
+++ generateFakeData.R
@@ -2,22 +2,23 @@
 
 library(dplyr)
 library(tidyverse)
-library(fakeR)
+library(hms)
 library(charlatan)
 
 ## First Step: from some real data, create new fake data and aggregate them togheter
 ## Dataframe to work: cliente, classe_possibile, giorni_della_settimana, compagnia_aerea, tipo_di_aereoplano, aereoporto
 
-dfAereporti <- read.csv("./csv/Aereoporti.csv")
-dfTipoAereoplano <- read.csv("./csv/Tipo_Aereplano.csv")
-dfGiorniSettimana <- read.csv("./csv/Giorni_della_settimana.csv")
-dfClassi <- read.csv("./csv/Classe_Possibile.csv")
-dfAereoplani <- read.csv("./csv/Aereoplani.csv")
+dfAereporti <- read.csv("./csv/Aeroporto.csv")
+dfTipoAereoplano <- read.csv("./csv/TipoDiAeroplano.csv")
+dfGiorniSettimana <- read.csv("./csv/GiorniDellaSettimana.csv")
+dfClassi <- read.csv("./csv/ClassePossibile.csv")
+dfAereoplani <- read.csv("./csv/Aeroplano.csv")
+dfCompagniaAerea <- read.csv("./csv/CompagniaAerea.csv")
 
 
-fakeAereports <- simulate_dataset(dfAereporti, n=80)
-fakeAereports <- rbind(dfAereporti, fakeAereports) %>%
-  distinct(Airport_code, .keep_all = TRUE)
+#fakeAereports <- simulate_dataset(dfAereporti, n=80)
+#fakeAereports <- rbind(dfAereporti, fakeAereports) %>%
+#  distinct(Airport_code, .keep_all = TRUE)
 
 
 ## Second step
@@ -27,15 +28,18 @@ fakeAereports <- rbind(dfAereporti, fakeAereports) %>%
 generateCartesianProd <- function(n = 1, ...){
   karg <- list(...)
   tmp_df <- cross_df(karg) %>%
+            unique() %>%
             sample_n(n)
   return(tmp_df)
 }
 
-#
+# Funzione per creare custom ID secondo un template
+# # = è un cifra, ? = è un carattere alfabetico
 generateID <- function(n=1, id_format="##") {
   tmpVec = c()
   item = 1
   
+  #recursive function for generate a list of different ids
   aux <- function(i, id, n, vec) {
     (x <- BaseProvider$new())
     if (i <= n) {
@@ -47,34 +51,64 @@ generateID <- function(n=1, id_format="##") {
     }
   }
   
-  return(aux(item, id_format, n, tmpVec))
+  # uppercase all the alfa-character
+  result <- aux(item, id_format, n, tmpVec) %>%
+    lapply(., function(v) {
+      if(is.character(v))
+        return(toupper(v))
+      else
+        return(v)
+    })
+  
+  return(result)
 }
 
+# Funzione per generare orario nella forma "16:20"
+generateTime <- function(n=1, hour=0, minute=23) {
+  hours <- as.character(ch_integer(n = n, min = hour, max = 23))
+  minutes <- as.character(ch_integer(n = n, min = minute, max = 59))
+  return(paste(hours, minutes, sep = ":"))
+}
 
-
-
+# Funzione per generare orario "sensati" (ovvero che avvegano dopo) dato
+# in input un vettore formato da date del tipo "HH:MM"
+generateSensibleTime <- function(x=c()) {
+  lapply(x, function(v) {
+    l <- unlist(str_split(v, ":"))
+    t <- generateTime(hour = as.integer(l[1]),
+                      minute = as.integer(l[2]))
+    return(t)
+  }) %>%
+    unlist()
+}
 
 # Può decollare
-puoDecollare <- generateCartesianProd(50, Airplane_type_name = dfTipoAereoplano$Airplane_type_name,
-                                       Airport_code = dfAereporti$Airport_code)
+puoDecollare <- generateCartesianProd(50, tipo_aeroplano = dfTipoAereoplano$nome,
+                                       aeroporto = dfAereporti$codice)
 
 # Tratta
 # Tratta ID -> T####
-tratta <- generateCartesianProd(50, Aereporto_Arrivo = dfAereporti$Airport_code,
-                                Aereporto_Partenza = dfAereporti$Airport_code) %>%
-          unique()
+tratta <- generateCartesianProd(50, aeroporto_arrivo = dfAereporti$codice,
+                                aeroporto_partenza = dfAereporti$codice) %>%
+          subset(aeroporto_arrivo != aeroporto_partenza) # non lo stesso aereoporto di arrivo e partenza
 
 tratta$id <- generateID(nrow(tratta), id_format = "T####")
-tratta$orario_arrivo <- ch_unix_time(nrow(tratta))
-tratta$orario_partenza <- ch_unix_time(nrow(tratta))
+tratta$orario_partenza <- generateTime(nrow(tratta))
+tratta$orario_previsto_arrivo <- generateSensibleTime(tratta$orario_partenza)
+
+# Compagnia Aerea Aereoplano
+compagniaAereaAereoplano <- generateCartesianProd(50, compagnia_aerea = dfCompagniaAerea$nome,
+                                                  aereoplano = dfAereoplani$codice)
 
 
 
 # Clienti
 clients <- ch_generate('name', 'phone_number', n = 100, locale = "it_IT")
+colnames(clients) <- c("nome","telefono")
+clients$nome <- gsub(pattern = "Sig. |Sig.ra |Dott. ", replacement = "", x = clients$nome)
 clients$codice_fiscale <- generateID(n=nrow(clients), id_format = "??????##?##???#?")
-clients$name <- gsub(pattern = "Sig. |Sig.ra |Dott. ", replacement = "", x = clients$name)
-clients <- clients %>% separate("name", c("name","surname"), extra = "merge")
+clients <- clients %>% separate("nome", c("nome","cognome"), extra = "merge")
+clients <- clients[, c("codice_fiscale","nome","cognome","telefono")]
 
 
 # Classe di Volo
