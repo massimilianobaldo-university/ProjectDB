b2086cd67bde58127fda64361bc57d5f549053a4
zucchi99 <francesco.zuccato99@gmail.com>
2021-09-15 10:47:28 +0200

    views and functions

--- sql_files/5_functions.sql
+++ sql_files/5_functions.sql
@@ -1,4 +1,5 @@
 
+-- 1) Giorni della settimana con più voli
 create view conta_numero_voli_per_giorno_settimana(giorno_settimana, numero_voli)
 as
 select 
@@ -7,6 +8,7 @@ select
 from giornidellasettimana_volo
 group by giorno_settimana;
 
+-- 2) Numero medio di passeggeri nei voli
 create view posti_rimanenti_info(tratta, data_istanza, num_posti_totali, num_posti_rimanenti, num_posti_prenotati, perc_posti_prenotati)
 as
 select 
@@ -21,3 +23,24 @@ join aeroplano as A on (
     IT.aeroplano = A.codice
 )
 order by data_istanza, perc_posti_prenotati;
+
+-- 3) Percentuale media di occupazione degli aerei
+
+-- 4) Le compagnie aeree più economiche
+create view prezzi_voli_compagnia_aerea(volo, classe, prezzo, compagnia_aerea)
+as
+select volo, classe, prezzo, compagnia_aerea
+from ClasseDiVolo CV
+join Volo V on CV.volo = V.codice;
+
+select 
+    sum(prezzo) as costo_totale, 
+    (count(*)/3) as numero_voli, 
+    trunc(((sum(prezzo))::decimal / ((count(*)/3))), 2) as costo_medio,
+    compagnia_aerea
+from prezzi_voli_compagnia_aerea
+group by compagnia_aerea
+order by costo_medio desc;
+
+
+
